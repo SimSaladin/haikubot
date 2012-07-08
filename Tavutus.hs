b2428d68fd2d1c643713ada488ef26123e345669
SimSaladin <simsaladin@paivola.fi>
2012-07-08 16:48:24 +0300

    initial commmit of working software

---
+++ Tavutus.hs
@@ -0,0 +1,82 @@
+------------------------------------------------------------------------------
+-- File:          Tavutus.hs
+-- Creation Date: Jul 06 2012
+-- Last Modified: Jul 08 2012 [14:41:49]
+-- Created By :   Samuli Thomasson [SimSaladin] samuli.thomassonATgmail.com
+------------------------------------------------------------------------------
+
+module Tavutus where
+
+-- 1. Konsonanttisääntö: Jos tavuun kuuluvaa vokaalia seuraa yksi tai useampia
+--    konsonantteja,joita vielä seuraa vokaali,tavuraja sijoittuu välittömästi
+--    ennen viimeistä konsonanttia.
+--
+-- 2. Vokaalisääntö: Jos tavun ensimmäistä vokaalia seuraa toinen vokaali,
+--    niiden väliin tulee tavuraja,ellei
+--    a) edellinen vokaali ole sama kuin jälkimmäinen (pitkä vokaali).
+--    b) jälkimmäinen vokaali ole i (i:hin loppuva diftongi).
+--    c) kysymyksessä ole jokin vokaalipareista au,eu,ie,iu,ou,uo,yö,äy,
+--    öy,ey tai iy (muu diftongi).
+--
+-- 3. Diftongisääntö: Jos tavun kuuluvaa difta tai pitkää vokaalia seuraa
+-- vokaali,tähän väliin tulee aina tavuraja.
+--
+-- 4. Poikkeussääntö: Yhdyssanat jaetaan tavuihin sanojen välistä,myös siinä
+-- tapauksessa,että sana on yhdyssana vain alkuperäiskielessä.
+--
+-- references: http://teppo.tv/haikueditori/tavutus.html
+--             http://fi.wikipedia.org/wiki/Tavu
+--
+
+import Text.ParserCombinators.Parsec
+import Data.List (delete)
+
+kons = "BCDFGHJKLMNPRSTVZXbcdfghjklmnŋprsštvzžx"
+voks = "AEIOUYÄÖaeiouyäö"
+
+printTavut :: [[[String]]] -> String
+printTavut s = foldl1 (\x y -> x ++ "; " ++ y) (map psae s)
+   where
+      psae :: [[String]] -> String
+      psae []  = ""
+      psae sae = foldl1 (\x y -> x ++ " " ++ y) (map psana sae)
+
+      psana :: [String] -> String
+      psana []   = ""
+      psana sana = foldl1 (\x y -> x ++ "-" ++ y) sana
+
+kon = oneOf kons
+vok = oneOf voks
+dift = foldl1 (<|>) (map (try . string) ["ai","ei","oi","ui","yi","äi","öi"
+                                ,"au","eu","iu","ou","ie","uo","yö"
+                                ,"äy","öy","ey","iy"])
+--dift = foldl1 (<|>) (map (\(x, y) -> string (x:[y])) (zip "aeouy\228\246aeiouy\228\246ei"
+--                                                          "iiiiiiiuuuuo\246yyyy"))
+
+vokDouble = foldl1 (<|>) (map (\x -> string (x:[x])) voks)
+
+tavutaRuno :: String -> Either String [[[String]]]
+tavutaRuno input = case parse parseRuno "" ((dropWhile (== ' ') input) ++ " ") of
+   Left err -> Left "Osaatko edes kirjoittaa?"
+   Right val -> Right $ delete [] val
+
+parseRuno :: Parser [[[String]]]
+parseRuno = sepEndBy parseTavutaSanat (many1 (many space >> many1 (char ';') >> many space))
+
+parseTavutaSanat :: Parser [[String]]
+parseTavutaSanat = sepEndBy (many1 parseTavu) (many1 space)
+
+parseTavu :: Parser String
+parseTavu = do
+   ks <- many kon         
+   v  <- try dift <|> try vokDouble <|> fmap return vok 
+   ls <- try loppuuKon <|> many (try (kon `followedByY` (kon >> (try kon <|> vok))))
+   puncts <- many (noneOf (kons ++ voks ++ " ;"))
+   return (ks ++ v ++ ls ++ puncts)
+
+loppuuKon :: Parser String
+loppuuKon = many kon >>= \r -> try $ lookAhead (noneOf (kons ++ voks)) >> return r
+
+followedByY :: Parser Char -> Parser Char -> Parser Char
+followedByY x y = x >>= \r -> lookAhead y >> return r
+
