74e40f5dfc3af72d61cb6319bbd03295859766d3
SimSaladin <simsaladin@paivola.fi>
2012-07-28 17:28:53 +0300

    add mplayer, modify mainplus

---
+++ MPlayer.hs
@@ -0,0 +1,14 @@
+------------------------------------------------------------------------------
+-- File: MPlayer.hs
+-- Creation Date: Jul 23 2012 [11:10:22]
+-- Last Modified: Jul 25 2012 [06:52:46]
+-- Created By: Samuli Thomasson [SimSaladin] samuli.thomassonAtpaivola.fi
+------------------------------------------------------------------------------
+module MPlayer where
+
+import Prelude
+
+-- | Opens file in mplayer and returns XXX:what?
+play :: FilePath -> IO ()
+play fp = do
+    return ()
